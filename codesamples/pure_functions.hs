c03ec7b7343b88058141afb788357d682b558f14
ATHUL S NAMBIAR <athulsnambiarap@yahoo.com>
2019-08-13 02:37:40 +0530

    added functions for add mult and exp

--- codesamples/pure_functions.hs
+++ codesamples/pure_functions.hs
@@ -1,4 +1,20 @@
-zerooo f x = x
-one f x = f x
-two f x = f(f x)
+zero :: Num a => (a -> a) -> a -> a
+
+zero f x = x
+
+one f x   = f x
+two f x   = f(f x)
 three f x = f(f(f x))
+four f x  = f(f(f(f x)))
+five f x  = f(f(f(f(f x))))
+six f x   = f(f(f(f(f(f x)))))
+seven f x = f(f(f(f(f(f(f x))))))
+eight f x = f(f(f(f(f(f(f(f x)))))))
+nine f x  = f(f(f(f(f(f(f(f(f x))))))))
+ten f x   = f(f(f(f(f(f(f(f(f(f x)))))))))
+
+add m n f x = n f (m f x)
+
+mult m n f x = m (n f) x
+
+expnentiation m n f x = n m f x
